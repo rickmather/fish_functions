770acb68815bb20c445c4deeeab9a17e161eee7f
Rick Mather <rickmather@economist.com>
2023-09-07 16:29:57 +0100

    feat: add nvm support

---
+++ sudo.fish
@@ -0,0 +1,8 @@
+# run `sudo !!` to repeat previous command with elevated privileges
+function sudo
+    if test "$argv" = !!
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
